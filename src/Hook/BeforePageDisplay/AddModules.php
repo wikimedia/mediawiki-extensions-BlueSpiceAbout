f1bda1d505a4f191e2bc97030b162116c0c654b8
dvogel <daniel.vogel@hallowelt.com>
2018-10-16 12:34:27 +0200

    Add module styles without checking config
    Change-Id: I169aaa5c4a7288ae8ef2d8f44b7a58dd46d136e5

--- src/Hook/BeforePageDisplay/AddModules.php
+++ src/Hook/BeforePageDisplay/AddModules.php
@@ -6,10 +6,6 @@ use BlueSpice\Hook\BeforePageDisplay;
 
 class AddModules extends BeforePageDisplay {
 
-	protected function skipProcessing() {
-		return !$this->getConfig()->get( 'BlueSpiceAboutShowMenuLinks' );
-	}
-
 	protected function doProcess() {
 		$this->out->addModuleStyles( 'ext.bluespice.bluespiceabout' );
 		return true;
