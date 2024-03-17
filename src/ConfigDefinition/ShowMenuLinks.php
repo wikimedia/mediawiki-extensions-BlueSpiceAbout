f6f3e271be8d811649c450cbb7fcd255be65d9b0
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-03-17 14:04:31 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 43.0.0
    Change-Id: I63c5e5ce16308532178c5ad509ccf785494dde22

--- src/ConfigDefinition/ShowMenuLinks.php
+++ src/ConfigDefinition/ShowMenuLinks.php
@@ -11,7 +11,7 @@ class ShowMenuLinks extends \BlueSpice\ConfigDefinition\BooleanSetting {
 	public function getPaths() {
 		return [
 			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_SEARCH . '/BlueSpiceAbout',
-			static::MAIN_PATH_EXTENSION . '/BlueSpiceAbout/' . static::FEATURE_SEARCH ,
+			static::MAIN_PATH_EXTENSION . '/BlueSpiceAbout/' . static::FEATURE_SEARCH,
 			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/BlueSpiceAbout',
 		];
 	}
