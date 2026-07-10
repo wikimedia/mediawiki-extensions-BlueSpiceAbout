0440744a7f0618c7b15432c84f52d6330ea73b2b
Hamish <hamish.slater@hallowelt.com>
2026-07-10 21:51:39 +0700

    phpcs
    Change-Id: I71a7d0dc4d3b979d01b9d0e77e69d319d9f5b4f5

--- includes/specials/SpecialBlueSpiceAbout.php
+++ includes/specials/SpecialBlueSpiceAbout.php
@@ -2,19 +2,6 @@
 
 use MediaWiki\Html\TemplateParser;
 
-/**
- * Renders the BlueSpice About special page.
- *
- * Part of BlueSpice MediaWiki
- *
- * @author     Markus Glaser <glaser@hallowelt.com>
- *
- * @package    BlueSpiceAbout
- * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
- * @filesource
- */
-
 class SpecialBlueSpiceAbout extends \BlueSpice\SpecialPage {
 
 	/**
